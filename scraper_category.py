9f3337f970c2e8adf3770653c87053eae5e61658
Marcin <148549131+MarcinekWr@users.noreply.github.com>
2024-07-20 00:04:08 +0200

    Update scraper_category.py

--- scraper_category.py
+++ scraper_category.py
@@ -1,59 +1,66 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
 from bs4 import BeautifulSoup
-import pandas as pd
-import json
-import os
-import numpy as np
-import re
-from bs4 import BeautifulSoup
-import requests
 import time
-import datetime
-import sqlalchemy as sa
 
-##SCRAPER WSYZSTKICH PODSTAWOWYCH KATEGORI Z LINKA https://www.amazon.pl/gp/bestsellers/ref=zg_bs_tab_bs
+
 def SCRAPER(URL):
+    # configuration webpage without graphical interface
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
-    service = ChromeService(executable_path=ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service,options=options)
 
+    # Initialize chrome browser
+    service = ChromeService(executable_path=ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+
+    # Open URL
     driver.get(URL)
-    page_source = driver.page_source
+
     time.sleep(5)
 
+    # Get the page source
+    page_source = driver.page_source
+
+    # Close browser
     driver.quit()
 
+    # Processing source using BeautyfulSoup
     soup = BeautifulSoup(page_source, 'html.parser')
 
     return soup
 
-def cathegory():
-    soup = SCRAPER("https://www.amazon.pl/gp/bestsellers/ref=zg_bs_tab_bs") ##GŁOWNA STRONA AMAZONA BESTSELLERS
 
-    divs = soup.find_all('div',class_='_p13n-zg-nav-tree-all_style_zg-browse-item__1rdKf _p13n-zg-nav-tree-all_style_zg-browse-height-small__nleKL')
+def cathegory():
+    # Get the main Amazon Bestsellers page using the SCRAPER function
+    soup = SCRAPER("https://www.amazon.pl/gp/bestsellers/ref=zg_bs_tab_bs")
+
+    # Find all divs containing category links
+    divs = soup.find_all('div',
+                         class_='_p13n-zg-nav-tree-all_style_zg-browse-item__1rdKf _p13n-zg-nav-tree-all_style_zg-browse-height-small__nleKL')
 
     links = []
+
+    # Loop through each div to extract the category name and link
     for div in divs:
         data = div.find('a')
-        name = data.text.strip()
-        link = 'https://www.amazon.pl/' + data['href']
+        name = data.text.strip()  # category name
+        link = 'https://www.amazon.pl/' + data['href']  # link
         links.append([name, link])
 
-    #MOJE DODATKI
-    additional_url =[
-        ['Tablety','https://www.amazon.pl/gp/bestsellers/electronics/20788300031/ref=zg_bs_nav_electronics_1'],
-        ['Laptotpy','https://www.amazon.pl/gp/bestsellers/electronics/20788292031/ref=zg_bs_nav_electronics_2_20788300031'],
-        ['Telefony komórkowe','https://www.amazon.pl/gp/bestsellers/electronics/20788267031/ref=zg_bs_nav_electronics_2_20788252031'],
-        ['Podzespoły i części komputerowe','https://www.amazon.pl/gp/bestsellers/electronics/20788298031/ref=zg_bs_nav_electronics_2_20788256031']
+    # My few additional links
+    additional_url = [
+        ['Tablety', 'https://www.amazon.pl/gp/bestsellers/electronics/20788300031/ref=zg_bs_nav_electronics_1'],
+        ['Laptotpy',
+         'https://www.amazon.pl/gp/bestsellers/electronics/20788292031/ref=zg_bs_nav_electronics_2_20788300031'],
+        ['Telefony komórkowe',
+         'https://www.amazon.pl/gp/bestsellers/electronics/20788267031/ref=zg_bs_nav_electronics_2_20788252031'],
+        ['Podzespoły i części komputerowe',
+         'https://www.amazon.pl/gp/bestsellers/electronics/20788298031/ref=zg_bs_nav_electronics_2_20788256031']
     ]
 
+    # Append the additional URLs to the links list
     links = links + additional_url
 
+    # Return the complete list of category names and links
     return links
